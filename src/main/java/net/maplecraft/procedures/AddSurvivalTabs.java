2128d479923295e17b24757555b471d925647bf3
Guojun Chen <guojun@Guojuns-MacBook-Pro.local>
2022-12-02 21:24:17 -0500

    update

--- src/main/java/net/maplecraft/procedures/AddSurvivalTabs.java
+++ src/main/java/net/maplecraft/procedures/AddSurvivalTabs.java
@@ -1,6 +1,8 @@
 package net.maplecraft.procedures;
 
 import net.maplecraft.MapleCraftMod;
+import net.minecraft.client.gui.screens.inventory.InventoryScreen;
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -9,6 +11,12 @@ import net.minecraft.client.gui.screens.Screen;
 public class AddSurvivalTabs {
     @SubscribeEvent
     public static void onSurvivalInventoryScreenInit(ScreenEvent.Init.Pre event) {
-        System.out.println("######## Open screen: " + event.getScreen().getTitle());
+        Screen screen = event.getScreen();
+        Player player = event.getScreen().getMinecraft().player;
+        if (screen instanceof InventoryScreen && player != null && !player.getAbilities().instabuild) {
+            System.out.println("######## Open survival screen: " + event.getScreen().getTitle());
+
+        }
+
     }
 }

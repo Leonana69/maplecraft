45b9978481c25a08394ae2d688fd33d7025588dc
leonana69 <chengj0803@gmail.com>
2022-12-02 23:27:57 -0500

    add inventory button

--- src/main/java/net/maplecraft/network/SkillMenuKeyMessage.java
+++ src/main/java/net/maplecraft/network/SkillMenuKeyMessage.java
@@ -61,7 +61,6 @@ public class SkillMenuKeyMessage {
                 }
             }, blockPos);
         }
-
     }
 
     @SubscribeEvent
--- src/main/java/net/maplecraft/procedures/AddSurvivalTabs.java
+++ src/main/java/net/maplecraft/procedures/AddSurvivalTabs.java
@@ -1,22 +1,30 @@
 package net.maplecraft.procedures;
 
 import net.maplecraft.MapleCraftMod;
+import net.maplecraft.network.SkillMenuKeyMessage;
+import net.maplecraft.world.customGUI.MapleButton;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.gui.screens.inventory.InventoryScreen;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraft.client.gui.screens.Screen;
+
 @Mod.EventBusSubscriber
 public class AddSurvivalTabs {
+
     @SubscribeEvent
-    public static void onSurvivalInventoryScreenInit(ScreenEvent.Init.Pre event) {
+    public static void onSurvivalInventoryScreenInit(ScreenEvent.Init.Post event) {
+        Minecraft mc = Minecraft.getInstance();
         Screen screen = event.getScreen();
         Player player = event.getScreen().getMinecraft().player;
         if (screen instanceof InventoryScreen && player != null && !player.getAbilities().instabuild) {
-            System.out.println("######## Open survival screen: " + event.getScreen().getTitle());
-
+            AbstractContainerScreen<?> gui = (AbstractContainerScreen<?>) screen;
+            event.addListener(new MapleButton(gui.getGuiLeft(), gui.getGuiTop() - 15,
+                    0, 0,
+                    e -> MapleCraftMod.PACKET_HANDLER.sendToServer(new SkillMenuKeyMessage(0, 0))));
         }
-
     }
 }
